56ed2dcc333a60d7df667df33253308bed680d62
ZuraKevanishvili <z_kevanishvili@cu.edu.ge>
2023-12-17 21:37:01 +0400

    Add project files.

---
+++ Project/DBcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+
+namespace Project
+{
+    public class DBcontext : DbContext
+    {
+        public DBcontext(DbContextOptions<DBcontext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
